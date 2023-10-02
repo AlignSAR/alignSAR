239ee94782f2e2cf05c106689fb296a4a54d418f
LC-SAR <76875977+LC-SAR@users.noreply.github.com>
2023-10-02 10:30:54 +0200

    Add files via upload

---
+++ itc-doris_5_patch2023/doris/prepare_stack/create_doris_input_xml.py
@@ -0,0 +1,163 @@
+import xml.etree.ElementTree as ET
+import os
+from datetime import datetime
+
+class CreateDorisInputXml(object):
+
+    def __init__(self, input_file):
+        self.input_file_dict={}
+        self.dem_folder=''
+        if(len(input_file)==0):
+            self._create_xml()
+        else:
+            self._read_xml(input_file)
+
+
+    def _create_xml(self):
+        # This will first create the framework with data folders the stackfolder should contain
+        # a dorisparameters file.
+
+        input = False
+        while input == False:
+            user_input = raw_input("Enter the path to the archive data folder: ")
+            if os.path.exists(user_input):
+                self.input_file_dict['sar_data_folder'] = user_input
+                input = True
+            else:
+                print('The path is incorrect, use another path')
+
+        input = False
+        while input == False:
+            user_input = raw_input("Which polarisation do you want to use (vv,hh,vh,hv): ")
+            if user_input in ['vv', 'hh', 'vh', 'hv']:
+                self.input_file_dict['polarisation'] = user_input
+                input = True
+            else:
+                print('This polarisation does not exist')
+
+        input = False
+        while input == False:
+            user_input = raw_input("Which track do you want to work with? (explore on https://scihub.copernicus.eu/dhus/) : ")
+            try:
+                input = str(int(user_input)).zfill(3)
+                self.input_file_dict['track'] = user_input
+                input = True
+            except:
+                print('This track does not exist')
+
+        input = False
+        while input == False:
+            user_input = raw_input("Is this track ascending or descending? (asc/dsc) : ")
+            if user_input in ['asc', 'dsc']:
+                self.input_file_dict['direction'] = user_input
+                input = True
+            else:
+                print('Input should either be asc or dsc')
+
+        input = False
+        while input == False:
+            self.input_file_dict['datastack_folder'] = raw_input("Enter the path to the folder of new datastack: ")
+            if os.path.exists(self.input_file_dict['datastack_folder']):
+                input = True
+            else:
+                print('The path is incorrect, use another path')
+
+        input = False
+        while input == False:
+            self.input_file_dict['shape_file_path'] = raw_input("Enter full path to the shapefile: ")
+            if os.path.exists(self.input_file_dict['shape_file_path']) and self.input_file_dict['shape_file_path'].endswith('.shp'):
+                input = True
+            else:
+                print('The path is incorrect, use another path')
+
+        input = False
+        while input == False:
+            user_input = raw_input("Enter the path to the folder of the orbit files: ")
+            if os.path.exists(user_input):
+                self.input_file_dict['orbits_folder'] = user_input
+                input = True
+            else:
+                print('The path is incorrect, use another path')
+
+        input = False
+        while input == False:
+            user_input = raw_input("Do you want to generate the DEM file automaticly (Yes/No): ").lower()
+            if user_input == 'yes' or user_input == 'no':
+                self.input_file_dict['generate_dem'] = user_input
+                input = True
+            else:
+                print('You should use either yes or no')
+
+        input = False
+        while input == False:
+            self.input_file_dict['dem_processing_folder'] = raw_input("Enter path to the dem folder: ")
+            self.input_file_dict['dem_folder'] = os.path.join(self.input_file_dict['datastack_folder'], 'dem')
+            if os.path.exists(self.input_file_dict['dem_processing_folder']):
+                input = True
+            else:
+                print('The path is incorrect, use another path')
+
+        input = False
+        while input == False:
+            user_input = raw_input("Do you want to use parallel computing (Yes/No): ").lower()
+            if user_input == 'yes' or user_input == 'no':
+                self.input_file_dict['parallel'] = user_input
+                input = True
+            else:
+                print('You should use either yes of no')
+
+        if user_input == 'yes':
+            nodes = raw_input("How many cores do you want to use: ")
+            self.input_file_dict['cores'] = nodes
+
+        input = False
+        while input == False:
+            user_input = raw_input("What is the start date of your stack in yyyy-mm-dd (can be changed later): ").lower()
+            try:
+                date = datetime.strptime(user_input, '%Y-%m-%d')
+                self.input_file_dict['start_date'] = user_input
+                input = True
+            except:
+                print('Format not recognized, 01-01-2014 chosen')
+                self.input_file_dict['start_date'] = user_input
+
+        input = False
+        while input == False:
+            user_input = raw_input("What is the end date of your stack in yyyy-mm-dd (can be changed later): ").lower()
+            try:
+                date = datetime.strptime(user_input, '%Y-%m-%d')
+                self.input_file_dict['end_date'] = user_input
+                input = True
+            except:
+                print('Format not recognized, 01-01-2050 chosen')
+                self.input_file_dict['end_date'] = user_input
+
+        input = False
+        while input == False:
+            user_input = raw_input("What is the master date of your stack in yyyy-mm-dd (can be changed later): ").lower()
+            try:
+                date = datetime.strptime(user_input, '%Y-%m-%d')
+                self.input_file_dict['master_date'] = user_input
+                input = True
+            except:
+                print('Format not recognized, 01-01-2016 chosen. Check https://scihub.copernicus.eu/dhus/#/home for valid date')
+                self.input_file_dict['master_date'] = user_input
+
+        xml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'doris_input_template.xml')
+        tree = ET.parse(xml_file)
+        self.settings = tree.getroot()
+
+        for key in self.input_file_dict.keys():
+            self.settings.find('*/' + key).text = self.input_file_dict.get(key)
+
+        tree.write(os.path.join(self.input_file_dict['datastack_folder'], 'doris_input.xml'))
+
+        return self.input_file_dict
+
+    def _read_xml(self, input_file):
+        xml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), input_file)
+        tree = ET.parse(xml_file)
+        self.settings = tree.getroot()
+
+    def get_value(self, key):
+        return self.settings.find('*/' + key).text
---
+++ itc-doris_5_patch2023/doris/prepare_stack/prepare_datastack.py
@@ -0,0 +1,62 @@
+# Function created by Gert Mulder
+# Institute TU Delft
+# Date 9-11-2016
+# Part of Doris 5.0
+
+# This function makes a setup of the processing folder, based on a single shapefile.
+# Inputs are
+# - the satellite sensor used
+# - the shapefile
+# - the processing folder we want to create
+# - the dem source folder where the intermediate DEM data is stored
+
+import os
+from create_dem import CreateDem
+from create_inputfiles import CreateInputFiles
+from create_doris_input_xml import CreateDorisInputXml
+from create_datastack_bash import CreateBash
+import xml.etree.ElementTree as ET
+
+class PrepareDatastack(object):
+
+    def __init__(self):
+        return
+
+    def prepare(self, inputfile):
+
+        # This will first create the framework with data folders the stackfolder should contain
+        # a dorisparameters file.
+
+        doris_input_xml = CreateDorisInputXml(inputfile)
+
+        folders = ['input_files', 'stack', 'dem']
+        for foldername in folders:
+            if not os.path.exists(os.path.join(doris_input_xml.get_value('datastack_folder'), foldername)):
+                os.mkdir(os.path.join(doris_input_xml.get_value('datastack_folder'), foldername))
+
+        # Then create the dem file
+        password_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'install', 'doris_config.xml')
+        tree_doris = ET.parse(password_file)
+        settings_doris = tree_doris.getroot()
+        srtm_username = settings_doris.find('.usgs_username').text
+        srtm_password = settings_doris.find('.usgs_password').text
+        dem_out = os.path.join(doris_input_xml.get_value('dem_folder'), 'dem.raw')
+        dem_var = dem_out + '.var'
+        #print('1')
+        dem = CreateDem()
+        
+        if doris_input_xml.get_value('generate_dem'.lower())=='yes':
+            dem.create(doris_input_xml.get_value('shape_file_path'), dem_out, dem_var, resample=None,
+                                    doris_input=True, rounding=0.1, border=1.5,
+                                    data_folder=doris_input_xml.get_value('dem_processing_folder'), quality='SRTM1',
+                                    password=srtm_password, username=srtm_username)
+
+        ## Then create the inputfiles
+        inputfiles_folder = os.path.join(doris_input_xml.get_value('datastack_folder'), 'input_files')
+        xml_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'inputfile_template.xml')
+        satellite = 'sentinel-1'
+        CreateInputFiles(dem_var, xml_file, satellite).create(inputfiles_folder)
+
+        root_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        CreateBash().create(doris_input_xml.get_value('datastack_folder'), root_folder, doris_input_xml.get_value('cores'))
+
