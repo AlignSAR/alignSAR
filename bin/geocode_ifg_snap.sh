20267601802f54cdc8065a78d415e28afbedafbd
espiritocz <espiritocz@gmail.com>
2023-10-25 21:16:17 +0100

    Update geocode_ifg_snap.sh

--- bin/geocode_ifg_snap.sh
+++ bin/geocode_ifg_snap.sh
@@ -13,12 +13,13 @@ outputdir=$2
 outname=`basename $input | cut -d '.' -f1`
 
 #for x in Phase Intensity; do
-for bandname in `grep "<BAND_NAME>" $input 2>/dev/null | cut -d '>' -f2 | cut -d '<' -f 1`
+for bandname in `grep "<BAND_NAME>" $input 2>/dev/null | cut -d '>' -f2 | cut -d '<' -f 1`; do    # for all bands. but i,q do not work!
+ x=`echo $bandname | cut -d '_' -f 1`
 #bandname=`grep "<BAND_NAME>" $input 2>/dev/null | cut -d '>' -f2 | grep ^$x | cut -d '<' -f 1`
 #if [ ! -z $bandname ]; then
  echo "geocoding "$bandname
- /opt/snap/bin/gpt $SNAPGRAPHS/graph_geocoding_s1_geotiff.xml -Psourceband=$bandname -Poutput=$outputdir/$x.$outname.geo.temp.tif -Pinput=$input #>/dev/null 2>/dev/null
- gdal_translate -of GTiff -ot Float32 -co COMPRESS=DEFLATE -co PREDICTOR=3 $outputdir/$x.$outname.geo.temp.tif $outputdir/$x.$outname.geo.tif
- rm $outputdir/$x.$outname.geo.temp.tif
+ /opt/snap/bin/gpt $SNAPGRAPHS/graph_geocoding_s1_geotiff.xml -Psourceband=$bandname -Poutput=$outputdir/$x.$outname.geo.temp.tif -Pinput=$input >/dev/null 2>/dev/null
+ gdal_translate -of GTiff -ot Float32 -co COMPRESS=DEFLATE -co PREDICTOR=3 $outputdir/$x.$outname.geo.temp.tif $outputdir/$x.$outname.geo.tif  2>/dev/null
+ rm $outputdir/$x.$outname.geo.temp.tif 2>/dev/null
 #fi
 done
