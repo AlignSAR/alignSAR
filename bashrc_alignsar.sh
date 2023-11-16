713c2ad58bdec4788f4c1bcb2922f8674975f46d
espiritocz <espiritocz@gmail.com>
2023-11-16 13:55:05 +0000

    Update bashrc_alignsar.sh

--- bashrc_alignsar.sh
+++ bashrc_alignsar.sh
@@ -1,5 +1,5 @@
 ## Path
 export ALIGNSAR_PATH="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
 export PATH="$ALIGNSAR_PATH/bin:$PATH"
-export PYTHONPATH="$ALIGNSAR_PATH/rdrcode:$PYTHONPATH"
+export PYTHONPATH="$ALIGNSAR_PATH/rdrcode:$ALIGNSAR_PATH/python:$PYTHONPATH"
 export SNAPGRAPHS="$ALIGNSAR_PATH/snap_graphs"
