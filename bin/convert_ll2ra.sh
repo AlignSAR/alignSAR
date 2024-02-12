031b062ed2e4d058243655476eb179b984d743dd
Milan Lazecky <espiritocz@gmail.com>
2024-02-12 15:59:44 -0500

    rdrcode: optimized processing

--- bin/convert_ll2ra.sh
+++ bin/convert_ll2ra.sh
@@ -3,7 +3,7 @@
 V=""
 ingeo=geo2ra.grd
 trans=trans.dat
-outra=outra.grd
+outra=radarcoded.grd
 nsamples=$1
 nlines=$2
 # spacing=0.00027  # 30 m. More accurate but takes longer
@@ -26,12 +26,12 @@ rm $ingeo.0.grd $ingeo.ok.grd
 if [ ! -f llr.grd ]; then 
  echo "Common llr.grd file not found, regenerating (takes long)"
  gmt gmtconvert $trans -o2,3,0 -bi4f -bo3f > llr
- gmt surface llr `gmt gmtinfo llp -I$spacing -bi3f` -bi3f -I$spacing -T.50 -Gllr.grd $V # lla/llr is needed to be generated need only once!
+ time gmt surface llr `gmt gmtinfo llp -I$spacing -bi3f` -bi3f -I$spacing -T.50 -Gllr.grd $V # lla/llr is needed to be generated need only once!
 fi
 if [ ! -f lla.grd ]; then 
  echo "Common lla.grd file not found, regenerating (takes long)"
  gmt gmtconvert $trans -o2,3,1 -bi4f -bo3f > lla
- gmt surface lla `gmt gmtinfo llp -I$spacing -bi3f` -bi3f -I$spacing -T.50 -Glla.grd $V
+ time gmt surface lla `gmt gmtinfo llp -I$spacing -bi3f` -bi3f -I$spacing -T.50 -Glla.grd $V
 fi
 #
 echo "Linking coordinates and data values"
@@ -41,12 +41,17 @@ gmt grdtrack llpr -nl -Glla.grd -bi4f -bo5f > llpra
 gmt gmtconvert llpra -bi5f -bo3f -o3,4,2 > rap
 # R=`gmt gmtinfo rap -I1/1 -bi3f`
 R="-R0/"$nsamples"/0/"$nlines
-echo "Converting discrete nodes to regular grid"
-gmt xyz2grd rap $R -I1 -r -G$outra -bi3f
+#echo "Converting discrete nodes to regular grid"
+#gmt xyz2grd rap $R -I1 -r -G$outra -bi3f
+echo "Running NN interpolation through the irregular mesh" # setting the tolerance for 5 pixels, that should be fine here
+# test showed that -S5 is ~1 min., -S 10 is ~3.5 min. and in the end the loss of values was only 0.02%
+time gmt nearneighbor rap $R -G$outra -r -bi3f -I1 -S5
+
 # R="-R0/"`echo $R | cut -d '/' -f2`"/0/"`echo $R | cut -d '/' -f4`
 # finally nearest neighbour interpolation to fill the holes (zeroes are values now!)
-echo "Filling holes by NN interpolation"
-gmt grdfill $outra -An $R -G$outra.filled.grd
+# good hack but takes AGES (need to optimize above)
+#echo "Filling holes by NN interpolation"
+#time gmt grdfill $outra -An $R -G$outra.filled.grd
 
 echo "done, cleaning"
 # clean
