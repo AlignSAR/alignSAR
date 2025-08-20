0fcf2f435dd78872a75c1bdb3f17c8d4059cdf68
Bingquan Li <38463989+Bingquan-InSAR@users.noreply.github.com>
2025-08-20 17:28:22 +0200

    Update sec2hhmmss.py

--- alignsar/itc-doris_5_patch2023/doris/bin/sec2hhmmss.py
+++ alignsar/itc-doris_5_patch2023/doris/bin/sec2hhmmss.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python
-import os,sys,time
+#!/usr/bin/env python3
+import os
+import sys
+import time
 
 def usage():
-    print '\nUsage: python sec2hhmmss.py time'
-    print '  where time is the time of day in seconds.'
-    print ' '
-    print ' Example '
-    print ' sec2hhmmss.py 59800.445398'
-    print ' 16:36:40.393'
+    print('\nUsage: python sec2hhmmss.py time')
+    print('  where time is the time of day in seconds.')
+    print(' ')
+    print(' Example ')
+    print(' sec2hhmmss.py 59800.445398')
+    print(' 16:36:40.393')
 
 try:
-    timeOfDay  = sys.argv[1] 
-except:
-    print 'Unrecognized input'
+    timeOfDay = sys.argv[1]
+except Exception:
+    print('Unrecognized input')
     usage()
     sys.exit(1)
 
-timeOfDay  = float(sys.argv[1])
+timeOfDay = float(sys.argv[1])
 
-hh = timeOfDay//3600
-timeOfDay = timeOfDay%3600
-mm = timeOfDay//60
-ss = timeOfDay%60
-print "%d:%d:%f" %(hh,mm,ss)
+hh = timeOfDay // 3600
+timeOfDay = timeOfDay % 3600
+mm = timeOfDay // 60
+ss = timeOfDay % 60
+print(f"{int(hh)}:{int(mm)}:{ss:.3f}")
