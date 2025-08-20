35368bdf33f68c62346ac29561b58180f9214123
Bingquan Li <38463989+Bingquan-InSAR@users.noreply.github.com>
2025-08-20 17:33:04 +0200

    Update tsx_dump_header2doris.py

--- alignsar/itc-doris_5_patch2023/doris/bin/tsx_dump_header2doris.py
+++ alignsar/itc-doris_5_patch2023/doris/bin/tsx_dump_header2doris.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # live code for parsing of XML TSX file into python data structures
 # and from there into DORIS res file structure
@@ -8,28 +8,22 @@
 
 from lxml import etree
 import string, time, sys
-#import xml.etree.ElementTree as ElementTree
-#import types
+# import xml.etree.ElementTree as ElementTree
+# import types
 
 def usage():
-    print '\nUsage: python tsx_dump_header2doris.py tsx_XML_product > outputfile'
-    print '  where tsx_XML_product is the input filename'
-#    print '        outputfile      is the output DORIS resultfile'
+    print('\nUsage: python tsx_dump_header2doris.py tsx_XML_product > outputfile')
+    print('  where tsx_XML_product is the input filename')
+#    print('        outputfile      is the output DORIS resultfile')
 
 try:
     inputFileName  = sys.argv[1]
 #    outputFileName = sys.argv[2]
 #    outStream      = open(outputFileName,'w')
-except:
-    print 'Unrecognized input'
+except Exception:
+    print('Unrecognized input')
     usage()
     sys.exit(1)
-
-# input file
-#inputFileName  = '04091/04091.xml'
-#
-#outputFileName = 'inputFileName'
-
 
 inTree = etree.parse(inputFileName)
 
@@ -82,20 +76,20 @@ queryList = {\
 
 # temp variables and parameters
 container     = {}
-# containerTemp = {}        
 events        = ('end',)
 
 # functions : not sure if needed
 def fast_iter_string(context):
-    for event,elem in context:
+    for event, elem in context:
         return elem.text
 
 # works with lists
-def fast_iter_list(context,tag=''):
-    for event,elem in context:
-        return elem.iterchildren(tag=tag).next().text
+def fast_iter_list(context, tag=''):
+    for event, elem in context:
+        # Python 3: .__next__() 代替 .next()
+        return next(elem.iterchildren(tag=tag)).text
 
-def hms2sec(hmsString,convertFlag='int'):
+def hms2sec(hmsString, convertFlag='int'):
     # input hmsString syntax: XX:XX:XX.xxxxxx
     secString = int(hmsString[0:2])*3600 + \
         int(hmsString[3:5])*60 + \
@@ -107,83 +101,63 @@ def hms2sec(hmsString,convertFlag='int'):
     else:
         return int(secString)
 
-
 for key in queryList.keys():
-
     try:
-        vars()[key];
-    except KeyError or NameError:
-        vars()[key] = [];
+        vars()[key]
+    except KeyError:
+        vars()[key] = []
 
     queryListKey = queryList[key]
     for nodes in inTree.findall(queryListKey):
-#    for nodes in inTree.findall(queryList[key]):
         vars()[key].append(nodes.text)
 
     container[key] = vars()[key]
-
 
 # ---------------------------------------------------------------------------------------------------------
 
 dummyVar = 'DUMMY'
 
-# outStream.write('MASTER RESULTFILE:        %s\n' % outputFileName)
-# outStream.write('\n')
-# outStream.write('\n')
-# outStream.write('Start_process_control\n')
-# outStream.write('readfiles:		1\n')
-# outStream.write('precise_orbits:         0\n')
-# outStream.write('crop:			0\n')
-# outStream.write('sim_amplitude:		0\n')
-# outStream.write('master_timing:		0\n')
-# outStream.write('oversample:		0\n')
-# outStream.write('resample:		0\n')
-# outStream.write('filt_azi:		0\n')
-# outStream.write('filt_range:		0\n')
-# outStream.write('NOT_USED:		0\n')
-# outStream.write('End_process_control\n')
-# outStream.write('\n')
 print('\ntsx_dump_header2doris.py v1.0, doris software, 2009\n')
 print('*******************************************************************')
 print('*_Start_readfiles:')
 print('*******************************************************************')
-print('Volume file: 					%s' % container['volFile'][0])
-print('Volume_ID: 					%s' % container['volID'][0])
-print('Volume_identifier: 				%s' % container['volRef'][0])
-print('Volume_set_identifier: 				%s' % dummyVar)
-print('(Check)Number of records in ref. file: 		%s' % container['sceneRecords'][0])
-print('SAR_PROCESSOR:                                  %s' % str.split(container['productSpec'][0])[0])
-print('Product type specifier: 	                %s' % container['mission'][0])
-print('Logical volume generating facility: 		%s' % container['productFacility'][0])
-print('Logical volume creation date: 			%s' % container['productVolDate'][0])
-print('Location and date/time of product creation: 	%s' % container['productDate'][0])
-print('Scene identification: 				Orbit: %s %s Mode: %s' % (container['orbitABS'][0],container['orbitDir'][0],container['sceneMode'][0]))
-print('Scene location: 		                lat: %.4f lon: %.4f' % (float(container['sceneCenLat'][0]),float(container['sceneCenLon'][0])))
-print('Leader file:                                 	%s' % container['volFile'][0])
-print('Sensor platform mission identifer:         	%s' % container['mission'][0])
-print('Scene_centre_latitude:                     	%s' % container['sceneCenLat'][0])
-print('Scene_centre_longitude:                    	%s' % container['sceneCenLon'][0])
-print('Radar_wavelength (m):                      	0.031') #HARDCODED!!!
-print('First_pixel_azimuth_time (UTC):			%s %s' % (time.strftime("%d-%b-%Y",time.strptime(container['azimuthTimeStart'][0].split('T')[0],"%Y-%m-%d")),container['azimuthTimeStart'][0].split('T')[1][:-1]))
-print('Pulse_Repetition_Frequency (computed, Hz): 	%s' % container['azimuthPRF'][0])
-print('Total_azimuth_band_width (Hz):             	%s' % container['azimuthBW'][0])
-print('Weighting_azimuth:                         	%s' % str.upper(container['azimuthWind'][0]))
-print('Xtrack_f_DC_constant (Hz, early edge):     	%s' % container['dopplerCoeff0'][0])
-print('Xtrack_f_DC_linear (Hz/s, early edge):     	%s' % container['dopplerCoeff1'][0])
+print('Volume file: \t\t\t\t\t%s' % container['volFile'][0])
+print('Volume_ID: \t\t\t\t\t%s' % container['volID'][0])
+print('Volume_identifier: \t\t\t\t%s' % container['volRef'][0])
+print('Volume_set_identifier: \t\t\t\t%s' % dummyVar)
+print('(Check)Number of records in ref. file: \t\t%s' % container['sceneRecords'][0])
+print('SAR_PROCESSOR: \t\t\t\t\t%s' % str.split(container['productSpec'][0])[0])
+print('Product type specifier: \t\t\t%s' % container['mission'][0])
+print('Logical volume generating facility: \t\t%s' % container['productFacility'][0])
+print('Logical volume creation date: \t\t\t%s' % container['productVolDate'][0])
+print('Location and date/time of product creation: \t%s' % container['productDate'][0])
+print('Scene identification: \t\t\t\tOrbit: %s %s Mode: %s' % (container['orbitABS'][0], container['orbitDir'][0], container['sceneMode'][0]))
+print('Scene location: \t\t\t\tlat: %.4f lon: %.4f' % (float(container['sceneCenLat'][0]), float(container['sceneCenLon'][0])))
+print('Leader file: \t\t\t\t\t%s' % container['volFile'][0])
+print('Sensor platform mission identifer: \t\t%s' % container['mission'][0])
+print('Scene_centre_latitude: \t\t\t\t%s' % container['sceneCenLat'][0])
+print('Scene_centre_longitude: \t\t\t%s' % container['sceneCenLon'][0])
+print('Radar_wavelength (m): \t\t\t\t0.031')  # HARDCODED!!!
+print('First_pixel_azimuth_time (UTC):\t\t\t%s %s' % (time.strftime("%d-%b-%Y", time.strptime(container['azimuthTimeStart'][0].split('T')[0], "%Y-%m-%d")), container['azimuthTimeStart'][0].split('T')[1][:-1]))
+print('Pulse_Repetition_Frequency (computed, Hz): \t%s' % container['azimuthPRF'][0])
+print('Total_azimuth_band_width (Hz): \t\t\t%s' % container['azimuthBW'][0])
+print('Weighting_azimuth: \t\t\t\t%s' % str.upper(container['azimuthWind'][0]))
+print('Xtrack_f_DC_constant (Hz, early edge): \t\t%s' % container['dopplerCoeff0'][0])
+print('Xtrack_f_DC_linear (Hz/s, early edge): \t\t%s' % container['dopplerCoeff1'][0])
 try:
-  print('Xtrack_f_DC_quadratic (Hz/s/s, early edge): 	%s' % container['dopplerCoeff2'][0])
-except:
-  print('Xtrack_f_DC_quadratic (Hz/s/s, early edge):    %s' % [0][0])
-print('Range_time_to_first_pixel (2way) (ms):     	%0.15f' % (float(container['rangeTimePix'][0])*1000))
-print('Range_sampling_rate (computed, MHz):       	%0.6f' % (float(container['rangeRSR'][0])/1000000))
-print('Total_range_band_width (MHz):               	%s' % (float(container['rangeBW'][0])/1000000))
-print('Weighting_range:                            	%s' % str.upper(container['rangeWind'][0]))
+    print('Xtrack_f_DC_quadratic (Hz/s/s, early edge): \t%s' % container['dopplerCoeff2'][0])
+except Exception:
+    print('Xtrack_f_DC_quadratic (Hz/s/s, early edge): \t%s' % [0][0])
+print('Range_time_to_first_pixel (2way) (ms): \t\t%0.15f' % (float(container['rangeTimePix'][0]) * 1000))
+print('Range_sampling_rate (computed, MHz): \t\t%0.6f' % (float(container['rangeRSR'][0]) / 1000000))
+print('Total_range_band_width (MHz): \t\t\t%s' % (float(container['rangeBW'][0]) / 1000000))
+print('Weighting_range: \t\t\t\t%s' % str.upper(container['rangeWind'][0]))
 print('')
 print('*******************************************************************')
-print('Datafile: 					%s' % container['imageData'][0])
-print('Dataformat: 				%s' % 'TSX_COSAR')  # hardcoded!!!
-print('Number_of_lines_original: 			%s' % container['imageLines'][0])
-print('Number_of_pixels_original: 	                %s' % container['imagePixels'][0])
+print('Datafile: \t\t\t\t\t%s' % container['imageData'][0])
+print('Dataformat: \t\t\t\t\t%s' % 'TSX_COSAR')  # hardcoded!!!
+print('Number_of_lines_original: \t\t\t%s' % container['imageLines'][0])
+print('Number_of_pixels_original: \t\t\t%s' % container['imagePixels'][0])
 print('*******************************************************************')
 print('* End_readfiles:_NORMAL')
 print('*******************************************************************')
@@ -192,24 +166,18 @@ print('')
 print('*******************************************************************')
 print('*_Start_leader_datapoints')
 print('*******************************************************************')
-print(' t(s)		X(m)		Y(m)		Z(m)')
-print('NUMBER_OF_DATAPOINTS: 			%s' % len(container['orbitTime']))
+print(' t(s)\t\tX(m)\t\tY(m)\t\tZ(m)')
+print('NUMBER_OF_DATAPOINTS: \t\t\t%s' % len(container['orbitTime']))
 print('')
 
 for i in range(len(container['orbitTime'])):
-    print(' %s %s %s %s' % (hms2sec(container['orbitTime'][i].split('T')[1]),\
-                                  container['orbitX'][i],\
-                                  container['orbitY'][i],\
-                                  container['orbitZ'][i]))
+    print(' %s %s %s %s' % (hms2sec(container['orbitTime'][i].split('T')[1]),
+                            container['orbitX'][i],
+                            container['orbitY'][i],
+                            container['orbitZ'][i]))
 
 print('')
 print('*******************************************************************')
 print('* End_leader_datapoints:_NORMAL')
 print('*******************************************************************')
-# print('\n')
-# print('    Current time: %s\n' % time.asctime())
-# print('\n')
-
-# close output file
-#outStream.close()
-
+# EOF
